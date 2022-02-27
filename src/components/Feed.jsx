3690b30cf6c7a91afb2cdf09babc0563a2d6e250
Sajan Madaan <madanraghav7@gmail.com>
2022-02-27 13:48:18 +0530

    ...

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,12 +3,14 @@ import "./CSS/Feed.css"
 import QuoraBox from './QuoraBox'
 import  {Post}  from './Post.jsx'
 import {Showques} from "./show_ques.jsx"
+import { Showpost } from './show_post'
 
 function Feed() {
   return (
     <div className="feed">
       <QuoraBox />
       <Showques />
+      <Showpost />
       <Post />
     </div>
   );
